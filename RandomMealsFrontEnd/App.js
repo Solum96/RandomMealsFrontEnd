bc8dc2182cec6a1d8a3a8ff2a9f1893729f8d6d3
Nils Lundell <Nils.Lundell@mardigras.se>
2021-03-16 15:56:01 +0100

    Started playing around with react for learning purposes
    Gotta stay educated

--- RandomMealsFrontEnd/App.js
+++ RandomMealsFrontEnd/App.js
@@ -1,12 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { render } from 'react-dom';
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
 
 export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    return (
+      <SafeAreaView style={styles.container}>
+        <Text>
+          Open up App.js to start working on your app!
+        </Text>
+        <Image source={require("./assets/favicon.png")}/>
+        <StatusBar style="auto" />
+
+        <View style={styles.row}>
+          <View style={styles.itemCard}>
+            <Text>
+              Id: 1
+              Namn: Nils Lundell
+            </Text>
+          </View>
+          <View style={styles.itemCard}>
+            <Text>
+              Id: 1
+              Namn: Nils Lundell
+            </Text>
+          </View>
+        </View>
     </SafeAreaView>
   );
 }
@@ -18,4 +37,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  row: {
+    flex: 0.2,
+    backgroundColor: 'gray',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    width: '100%',
+    flexDirection: 'row',
+  },
+  itemCard: {
+    margin: 5,
+    flex: 0.5,
+    alignItems: 'center',
+    width: '33%',
+  }
 });
