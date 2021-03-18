b9f4f83d3fd914a6b8810330218b006c2886d3db
Nils Lundell <Nils.Lundell@mardigras.se>
2021-03-18 11:02:53 +0100

    Added some icons to tab nav

--- RandomMealsFrontEnd/App.js
+++ RandomMealsFrontEnd/App.js
@@ -4,6 +4,7 @@ import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
 import { Home } from "./screens/Home/Home";
 import { Details } from "./screens/Details/Details";
 
@@ -12,7 +13,28 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === "Home") {
+              iconName = focused
+                ? "ios-information-circle"
+                : "ios-information-circle-outline";
+            } else if (route.name === "Details") {
+              iconName = focused ? "ios-list" : "ios-list";
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: "tomato",
+          inactiveTintColor: "gray",
+        }}
+      >
         <Tab.Screen name="Home" component={Home} />
         <Tab.Screen name="Details" component={Details} />
       </Tab.Navigator>
