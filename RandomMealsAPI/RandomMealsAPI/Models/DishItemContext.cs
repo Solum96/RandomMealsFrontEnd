4c210a3630a2ff42867310048562938ac5fc56b0
Nils Lundell <nils.solumfaeste@gmail.com>
2021-05-31 09:04:23 +0200

    Created basic REST api

---
+++ RandomMealsAPI/RandomMealsAPI/Models/DishItemContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace RandomMealsAPI.Models
+{
+    public class DishItemContext : DbContext
+    {
+        public DishItemContext(DbContextOptions<DishItemContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DishItem> DishItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DishItem>()
+            .Property(e => e.Ingredients)
+            .HasConversion(
+                v => string.Join(',', v),
+                v => v.Split(',', StringSplitOptions.RemoveEmptyEntries));
+
+            modelBuilder.Entity<DishItem>()
+            .Property(e => e.Instructions)
+            .HasConversion(
+                v => string.Join(',', v),
+                v => v.Split(',', StringSplitOptions.RemoveEmptyEntries));
+        }
+    }
+}
